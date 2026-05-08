aa35b8b1e57ed0e101dbc2ba3c70b6e6409b8506
Tinais24 <urtzigurkewicz@outlook.com>
2026-05-08 17:38:43 -0300

    Respondi as questões

--- exercicio-star-wars.ts
+++ exercicio-star-wars.ts
@@ -96,7 +96,7 @@ darthJar.apresentar();
 darthJar.usarHabilidade();
 
 // Droide
-console.log("\nDroide");
+console.log("\nDroide"); // B2-RP
 const b2RP = new Droide("Droid de Batalha B2-RP ", "Quell", 93, "Operações de ataque");
 b2RP.apresentar();
 b2RP.usarHabilidade();
@@ -106,5 +106,18 @@ console.log("\n");
 
 // Respotas das Questões
 
+// 1. Qual classe é a superclasse do exercício?
+    // A classe Personagem.
 
+// 2. Quais classes são subclasses?
+    // As classes Jedi, Sith e Droid.
+    
+// 3. Por que usamos super(...) no construtor das subclasses?
+    // O "super(...)" é utilizado para invocar o construtor da superclasse (Personagem), isso é obrigatório na herança para garantir que os atributos herdados, como por exemplo o nome, planeta, energia sejam corretamente inicializados antes da subclasse inicializar seus atributos exclusivos.
+
+// 4. Qual a diferença entre private e protected?
+    // O "private" restringe o acesso ao atributo ou método apenas dentro da própria classe onde ele foi declarado, já o "protected" também proíbe o acesso externo, porém, ele permite que o atributo/método seja acessado pelas subclasses (classes filhas).
+
+// 5. O que é sobrescrita de método?
+    // A sobrescrita é ativada quando uma subclasse recria (redefine) um método que ela herdou da superclasse, permitindo alterar o comportamento original do método para se adequar às necessidades específicas da subclasse, como foi feito no método "usarHabilidade().
 
