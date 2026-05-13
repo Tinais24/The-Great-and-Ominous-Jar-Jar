5d0ca29704d57967b9a62c7f1aeed32c025c65a9
Tinais24 <urtzigurkewicz@outlook.com>
2026-05-12 21:52:46 -0300

    Fiz o desafio do Mandaloriano

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # The-Great-and-Ominous-Jar-Jar
-Há muito tempo, em um repositório muito, muito distante...
+A long time ago, in a repository far, far away...
+
+
+Rodar os próximos comandos a seguir após instalar o Node.js:
+
+npm install -g typescript irá baixar o compilador TypeScript 
+
+tsc exercicio-star-wars.ts
+
+node exercicio-star-wars.js 
+
+Assim o código irá ser executado.
--- exercicio-star-wars.js
+++ exercicio-star-wars.js
@@ -1,148 +1,102 @@
+"use strict";
 // 1 - Super Classe do Personagem
-
 class Personagem {
-
-    protected nome: string;
-    protected planeta: string;
-    protected energia: number;
-
-    constructor(nome: string, planeta: string, energia: number) {
-
+    nome;
+    planeta;
+    energia;
+    constructor(nome, planeta, energia) {
         this.nome = nome;
         this.planeta = planeta;
         this.energia = energia;
     }
-
-    apresentar(): void {
-
+    apresentar() {
         console.log(`Nome: ${this.nome}`);
         console.log(`Planeta: ${this.planeta}`);
         console.log(`Energia: ${this.energia}`);
     }
-
-    usarHabilidade(): void {
-
+    usarHabilidade() {
         console.log("O personagem usou uma habilidade.");
     }
 }
-
 // 2 - Subclasses dos Personagens
-
 // Classe Jedi
 class Jedi extends Personagem {
-
-    private corSabre: string;
-
-    constructor(nome: string, planeta: string, energia: number, corSabre: string) {
+    corSabre;
+    constructor(nome, planeta, energia, corSabre) {
         super(nome, planeta, energia);
         this.corSabre = corSabre;
     }
-
     // Sobrescrevendo o método usarHabilidade
-
-    usarHabilidade(): void {
-
+    usarHabilidade() {
         console.log(`O Jedi ${this.nome} usou o Truque Mental.`);
         console.log(`Cor do sabre de luz: ${this.corSabre}`);
     }
 }
-
 // Classe Sith
 class Sith extends Personagem {
-
-    private nivelRaiva: number;
-
-    constructor(nome: string, planeta: string, energia: number, nivelRaiva: number) {
-
+    nivelRaiva;
+    constructor(nome, planeta, energia, nivelRaiva) {
         super(nome, planeta, energia);
         this.nivelRaiva = nivelRaiva;
     }
-
-    usarHabilidade(): void {
-
+    usarHabilidade() {
         console.log(`O Sith ${this.nome} usou a Aura Sombria.`);
         console.log(`Nível de raiva: ${this.nivelRaiva}`);
     }
 }
-
 // Classe Droide
 class Droide extends Personagem {
-
-    private funcao: string;
-
-    constructor(nome: string, planeta: string, energia: number, funcao: string) {
-
+    funcao;
+    constructor(nome, planeta, energia, funcao) {
         super(nome, planeta, energia);
         this.funcao = funcao;
     }
-
-    usarHabilidade(): void {
-
+    usarHabilidade() {
         console.log(`O droide ${this.nome} executou sua função: ${this.funcao}.`);
     }
 }
-
 // Classe Mandaloriano
 class Mandaloriano extends Personagem {
-
-    private armaPrincipal: string;
-
-    constructor(nome: string, planeta: string, energia: number, armaPrincipal: string) {
-        
+    armaPrincipal;
+    constructor(nome, planeta, energia, armaPrincipal) {
         super(nome, planeta, energia);
         this.armaPrincipal = armaPrincipal;
     }
-
-    usarHabilidade(): void {
-        
+    usarHabilidade() {
         console.log(`O Mandaloriano ${this.nome} atacou usando o ${this.armaPrincipal}.`);
     }
 }
-
-
 // 3 - Saída esperada aproximada
-
 // Jedi
 console.log("\nJedi"); // Obi-Wan
 const obiWan = new Jedi("Obi-Wan Kenobi", "Stewjon", 98, "Azul");
 obiWan.apresentar();
 obiWan.usarHabilidade();
-
 // Sith
 console.log("\nSith"); // Darth Jar-Jar
 const darthJar = new Sith("Darth Jar-Jar", "Naboo", 99, 100);
 darthJar.apresentar();
 darthJar.usarHabilidade();
-
 // Droide
 console.log("\nDroide"); // B2-RP
 const b2RP = new Droide("Droid de Batalha B2-RP ", "Quell", 93, "Operações de ataque");
 b2RP.apresentar();
 b2RP.usarHabilidade();
-
 // Mandaloriano (Desafio)
 console.log("\nMandaloriano"); // Pre Vizsla
 const mandaloriano1 = new Mandaloriano("Pre Vizsla", "Mandalore", 94, "Sabre Negro");
 mandaloriano1.apresentar();
 mandaloriano1.usarHabilidade();
-
 // Quebra de linha para deixar bonito
 console.log("\n");
-
 // Respotas das Questões
-
 // 1. Qual classe é a superclasse do exercício?
-    // A classe Personagem.
-
+// A classe Personagem.
 // 2. Quais classes são subclasses?
-    // As classes Jedi, Sith, Droid e Mandaloriano.
-    
+// As classes Jedi, Sith, Droid e Mandaloriano.
 // 3. Por que usamos super(...) no construtor das subclasses?
-    // O "super(...)" é utilizado para invocar o construtor da superclasse (Personagem), isso é obrigatório na herança para garantir que os atributos herdados, como por exemplo o nome, planeta, energia sejam corretamente inicializados antes da subclasse inicializar seus atributos exclusivos.
-
+// O "super(...)" é utilizado para invocar o construtor da superclasse (Personagem), isso é obrigatório na herança para garantir que os atributos herdados, como por exemplo o nome, planeta, energia sejam corretamente inicializados antes da subclasse inicializar seus atributos exclusivos.
 // 4. Qual a diferença entre private e protected?
-    // O "private" restringe o acesso ao atributo ou método apenas dentro da própria classe onde ele foi declarado, já o "protected" também proíbe o acesso externo, porém, ele permite que o atributo/método seja acessado pelas subclasses (classes filhas).
-
+// O "private" restringe o acesso ao atributo ou método apenas dentro da própria classe onde ele foi declarado, já o "protected" também proíbe o acesso externo, porém, ele permite que o atributo/método seja acessado pelas subclasses (classes filhas).
 // 5. O que é sobrescrita de método?
-    // A sobrescrita é ativada quando uma subclasse recria (redefine) um método que ela herdou da superclasse, permitindo alterar o comportamento original do método para se adequar às necessidades específicas da subclasse, como foi feito no método "usarHabilidade().
-
+// A sobrescrita é ativada quando uma subclasse recria (redefine) um método que ela herdou da superclasse, permitindo alterar o comportamento original do método para se adequar às necessidades específicas da subclasse, como foi feito no método "usarHabilidade().
